c63206a0dc149a7474332bcc9ac19ffcccd24986
Avanish <79317546+avanishbgit@users.noreply.github.com>
2023-04-24 14:24:34 +0530

    Producer now maintains state

--- producer.py
+++ producer.py
@@ -9,18 +9,22 @@ pushshift_api_endpoint = "https://api.pushshift.io/reddit/search/comment/"
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: json.dumps(x).encode('utf8'))
 user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
 
+recieved = []               # maintain a list of recieved comments
+
 def subreddit_request(api_endpoint):
     result = requests.get(api_endpoint, headers=user_agent).json()
     data = []
     if 'data' in result:
         for comment in result['data']:
-            data.append({
-                'subreddit': comment['subreddit'],
-                'id': comment['id'],
-                'body': comment['body'],
-                'created_utc': comment['created_utc'],
-                'utc_datetime_str': comment['utc_datetime_str'],
-            })
+            if comment['id'] not in recieved:
+                data.append({
+                    'subreddit': comment['subreddit'],
+                    'id': comment['id'],
+                    'body': comment['body'],
+                    'created_utc': comment['created_utc'],
+                    'utc_datetime_str': comment['utc_datetime_str'],
+                })
+                recieved.append(comment['id'])
     return data
 
 def publish_to_topic(topic_name, payload):
@@ -40,6 +44,7 @@ def main():
     #     "soccer_endpoint" : pushshift_api_endpoint + "?subreddit=soccer&size=10"
     # }
     # print(subreddits)
+
     while True:
         comments = subreddit_request(subreddits['askreddit_endpoint'])
         for comment in comments:
