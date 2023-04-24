f6ab74f29e50ab334e66ee335de67775da008efb
Manas Chebrolu <75514487+manasch@users.noreply.github.com>
2023-04-25 00:06:16 +0530

    batch processing something working

--- database.py
+++ database.py
@@ -26,7 +26,10 @@ consumer = KafkaConsumer("askreddit", bootstrap_servers=['localhost:9092'], valu
 for record in consumer:
     comment = record.value
 
-    query = f"insert into {table} values (id, subreddit, body, created_utc) values (%s, %s, %s, %s)"
-    cur.execute(f"insert into {table} values (\"{(comment['id'])}\",\"{(comment['subreddit'])}\", \"{(comment['body']).encode().hex()}\", {comment['created_utc']})")
-    mydb.commit()
-    print("Inserted")
+    try:
+        query = f"insert into {table} values (id, subreddit, body, created_utc) values (%s, %s, %s, %s)"
+        cur.execute(f"insert into {table} values (\"{(comment['id'])}\",\"{(comment['subreddit'])}\", \"{(comment['body']).encode().hex()}\", {comment['created_utc']})")
+        mydb.commit()
+        print("Inserted")
+    except Exception as e:
+        print(e)
